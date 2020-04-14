ad2a38d535a105c69384e5e8b4e3810443b7466f
Richard Atom <richardatom@aoa.local>
2020-04-14 12:37:19 +0200

    ServeFiles and MergeFiles

--- tempo.go
+++ tempo.go
@@ -4,13 +4,18 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
+	"net/http"
 	"path"
 	"strings"
+	"io/ioutil"
+	"path/filepath"
+	"os"
+	"log"
 )
 
 // GetTemplate has 3 inputs:
 // * template folder name (with path which relative to the project folder)
-// * template file anem (with path which relative to the template folder)
+// * template file name (with path which relative to the template folder)
 // * data for the template file
 func GetTemplate(viewPath string, filePath string, data interface{}) string {
 	var tplBuffer bytes.Buffer
@@ -33,4 +38,67 @@ func GetTemplate(viewPath string, filePath string, data interface{}) string {
 	}
 
 	return tplBuffer.String()
+}
+
+func visit(files *[]string, ftype string) filepath.WalkFunc {
+    return func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            log.Fatal(err)
+        }
+				if filepath.Ext(path) == ftype {
+
+	        *files = append(*files, path)
+				}
+				return nil
+    }
+}
+
+// MergeFiles has 4 inputs:
+// * folderPath (with path which relative to the project folder)
+// * contentType for the proper response header (eg.: "text/javascript; charset=utf-8")
+// * cacheAge cache max age as string
+// * request and response writers
+func MergeFiles(folderPath string, contentType string, cacheAge string, w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", contentType)
+	w.Header().Set("Cache-Control", "public, max-age=" + cacheAge)
+
+    var files []string
+		s := r.URL.Path[1:]
+    //fmt.Println(s)
+		s = strings.Replace(s, ".js", "", -1)
+    root := fmt.Sprintf("./_%s/", s)
+		err := filepath.Walk(root, visit(&files, ".js"))
+    if err != nil {
+			fmt.Println(err)
+      //panic(err)
+    }
+		//w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    for _, file := range files {
+        //fmt.Println(file)
+
+				b, err := ioutil.ReadFile(file) // just pass the file name
+				if err != nil {
+					fmt.Print(err)
+				}
+				str := string(b) // convert content to a 'string'
+
+				fmt.Fprintf(w, str)
+    }
+}
+
+func fileExists(filename string) bool {
+    info, err := os.Stat(filename)
+    if os.IsNotExist(err) {
+        return false
+    }
+    return !info.IsDir()
+}
+
+// ServeFiles is a dead-simple file serving helper
+func ServeFiles(w http.ResponseWriter, r *http.Request) {
+  if fileExists(r.URL.Path[1:]) {
+    http.ServeFile(w, r, r.URL.Path[1:])
+  } else {
+    http.Error(w, "Not Found", 404)
+  }
 }
